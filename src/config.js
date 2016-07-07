ce22031fa9334a119a23649755938781d0ec58c5
Levin Keller <post@levinkeller.de>
2016-07-07 13:27:52 +0200

    Reset.

--- src/config.js
+++
@@ -1,7 +0,0 @@
-export const FIREBASE_URL = 'https://reisekosten.firebaseio.com';
-
-// Route paths
-export const SIGN_IN_PATH = '/sign-in';
-export const TRIPS_PATH = '/trips';
-export const POST_SIGN_IN_PATH = TRIPS_PATH;
-export const POST_SIGN_OUT_PATH = SIGN_IN_PATH;
--- src/core/trips/index.js
+++
@@ -1,5 +0,0 @@
-import * as tripsActions from './actions';
-export { tripsActions };
-
-export * from './action-types';
-export * from './reducer';
